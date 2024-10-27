3801c18f8f2bf08fbb4ee22539f47ce92aad09da
Chirag Gupta <108172197+RealChiragOO7@users.noreply.github.com>
2024-10-27 20:19:20 +0530

    Initial Commit

---
+++ Linux/Arch/aur_packages.sh
@@ -0,0 +1,14 @@
+cd ~/Downloads
+
+ide="android-studio arduino-ide-bin visual-studio-code-bin rstudio-desktop-bin"
+general="anydesk-bin google-chrome rate-mirrors sticky-notes ttf-times-new-roman yay"
+
+all="$ide $general"
+for package in $all; do
+    echo "Installing $package"
+    git clone https://aur.archlinux.org/"$package".git
+    cd "$package" || exit
+    makepkg -si --noconfirm --needed
+    cd - || exit
+    rm -rf "$package"
+done
---
+++ Windows/uupdump/README.md
@@ -0,0 +1,3 @@
+# UUPDUMP
+
+Place these files in the zip downloaded from [UUPDUMP](https://uupdump.net/) to create custom windows ISO.
